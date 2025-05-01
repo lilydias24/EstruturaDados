b5f893bf95e0a3932cf140c7f9db4277d01782f2
Emilly Nascimento Dias <125706749+lilydias24@users.noreply.github.com>
2025-05-01 20:38:46 -0300

    Exs

---
+++ Exercicio_05/Aluno.java
@@ -0,0 +1,20 @@
+package Exercicio_05;
+
+public class Aluno {
+    private String nome;
+    private int matricula;
+
+    public Aluno(String nome, int matricula) {
+        this.nome = nome;
+        this.matricula = matricula;
+    }
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    @Override
+    public String toString() {
+        return "Aluno{" + "nome='" + nome + "', matricula=" + matricula + '}';
+    }
+}
